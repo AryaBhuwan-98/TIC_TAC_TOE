24a24829710f6636c7a524b594b1d6fd4b74b729
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-18 22:17:05 -0800

    Add:Adding program for write a method Showboard to display the current board

--- src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
+++ src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
@@ -3,20 +3,21 @@ package com.bridgelabz.tictactoeprogram;
 import java.util.Scanner;
 
 public class TicTacToe {
+    
     int i;
-	static char[] board = new char[10];  
+	static char[] board = new char[10]; 
 	static char player, computer;
 	Scanner scanner = new Scanner(System.in);
 
 	
-	 
 	public void initialize() {
-		for (i = 1; i < 10; i++) {   
+		for (i = 1; i < 10; i++) { 
 			board[i] = ' ';
 		}
 	}
+
 	
-	public void check() { 
+	public void check() { // creating method to select x & O
 		System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
 		char choice = scanner.next().charAt(0);
 		if (choice == 'X') {
@@ -27,16 +28,26 @@ public class TicTacToe {
 			computer = 'X';
 		}
 	}
-	
-	 
-	public static void main(String[] args) {
+
+	/*
+	 * Displaying the tic tac toe game board
+	 */
+	public void showboard() { // creating board
+
 		System.out.println("Welcome to Tic Tac Toe");
 		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
 		System.out.println("---------");
 		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
 		System.out.println("---------");
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-		
+
+	}
+
+	public static void main(String[] args) {
+
+		TicTacToe object = new TicTacToe();
+		object.initialize();
+		object.showboard();
+
 	}
 }
-
